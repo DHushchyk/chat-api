5d8dedd5eb6f1bfe7cdec308321735b62105987e
Dmytro Hushchyk <d.hushchyk@gmail.com>
2023-07-11 21:44:32 +0300

    changed db settings

--- core/settings.py
+++ core/settings.py
@@ -73,28 +73,17 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-if 'RDS_DB_NAME' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
-        }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_PSQL_NAME'),
+        'USER': os.getenv('DB_PSQL_USER'),
+        'PASSWORD': os.getenv('DB_PSQL_PASSWORD'),
+        'HOST': os.getenv('DB_PSQL_HOST'),
+        'PORT': os.getenv('DB_PSQL_PORT'),
     }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": "chat_api_db",
-            "USER": "chat_api_admin",
-            "PASSWORD": "OB9hh$r19SxY9fGmky$L",
-            "HOST": "localhost",
-            "PORT": "5432",
-        }
-    }
+}
 
 
 # Password validation
