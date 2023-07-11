e3f3468cb5dc2bd128ffe6359511a0de0a20d918
Dmytro Hushchyk <d.hushchyk@gmail.com>
2023-07-11 22:31:50 +0300

    added gunicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,4 @@ chmod -R o+rx /usr/src/app/media/
 chmod -R o+rx /usr/src/app/static/
 
 echo "# Starting server"
-#gunicorn --access-logfile - --workers 3 -t 300 --log-level debug --bind 0.0.0.0:8000 core.wsgi:application
 gunicorn --access-logfile - --workers 3 -t 300 --log-level debug --bind 0.0.0.0:8000 core.wsgi:application
